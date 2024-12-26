b69eccba176b5d4cb05eebe6d0eb1df18ee2ac88
Faizul karim <faizulkarim28@gmail.com>
2024-12-26 22:34:28 +0600

    Initial Commit

---
+++ SafeBrowser/SafeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeBrowserApp.swift
+//  SafeBrowser
+//
+//  Created by Md Faizul karim on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SafeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
